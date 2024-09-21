4b39b2d6895c1162d6a07c95c35ae873fc4c831d
deshmanepratik <deshmane.pratik@gmail.com>
2024-09-21 22:38:40 +0800

    testing default projectname tag

--- instance.tf
+++ instance.tf
@@ -17,7 +17,6 @@ module "first_module" {
     availability_zone = "us-east-1c"
     name_tag =  "frontendserver"
     project_number = "199119"
-    project_name = "frontend"
     instance_type = "t3.micro"
     ami = "ami-0e86e20dae9224db8"
 
