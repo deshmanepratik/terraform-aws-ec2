3785b3cb4d301a136242a2edc9b392a8fdb9d458
deshmanepratik <deshmane.pratik@gmail.com>
2024-09-21 22:25:48 +0800

    added code to read credentials from env vars

--- instance.tf
+++ instance.tf
@@ -9,11 +9,13 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 module "first_module" {
     source = "./modules/first_module"
-    
+
     availability_zone = "us-east-1c"
     name_tag =  "frontendserver"
     project_number = "199119"
